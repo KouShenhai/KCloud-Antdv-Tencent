3a1ed07e1d73b6832b4b65bf0822854af2697b18
koushenhai <2413176044@qq.com>
2022-11-21 12:35:12 +0800

    update:修改readme

--- src/utils/screenLog.js
+++ src/utils/screenLog.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export const printANSI = () => {
   // console.clear()
-  console.log('[KCloud-Antdv-Official] created()')
+  console.log('[KCloud-Antdv-Official] created')
   console.log('POWERED BY KCloud-Antdv-Official. Antd-Vue. Antd-Vue-Pro')
   // ASCII - ANSI Shadow
   let text = `               ███                      █                                  █                         ▒██    ▒██    █             █           ███   
@@ -16,6 +16,6 @@ export const printANSI = () => {
  █   ▒█  ▒███▒   ▒██   ███   ▒██▒█   ██▓█         █░  ░█ █   █    ▒██   ██▓█   ░█░           ▓██▓    █      █    █████   ▓██▒  █████  ▒██▒█    ▒██ 
 `
   console.log(`%c${text}`, 'color: #fc4d50')
-  console.log('%cGitee: https://gitee.com/tttt_wmh_cn/KCloud-Platform', 'color: #fff; font-size: 14px; font-weight: 300; text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;')
-  console.log('%cGitee: https://gitee.com/tttt_wmh_cn/KCloud-Antdv', 'color: #fff; font-size: 14px; font-weight: 300; text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;')
+  console.log('%cGitee: https://gitee.com/laokouyun/KCloud-Platform-Official', 'color: #fff; font-size: 14px; font-weight: 300; text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;')
+  console.log('%cGitee: https://gitee.com/laokouyun/KCloud-Antdv-Officail', 'color: #fff; font-size: 14px; font-weight: 300; text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;')
 }
